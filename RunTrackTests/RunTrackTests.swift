cd6641d5518e4488435159cebbde1b5663b43763
newuser <yespeace@bk.ru>
2025-09-25 17:09:50 +0600

    Initial Commit

---
+++ RunTrackTests/RunTrackTests.swift
@@ -0,0 +1,17 @@
+//
+//  RunTrackTests.swift
+//  RunTrackTests
+//
+//  Created by Damir Aushenov on 25/9/25.
+//
+
+import Testing
+@testable import RunTrack
+
+struct RunTrackTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
